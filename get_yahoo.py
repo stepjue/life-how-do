53302b6e2b67d221235d72d6bc00562fcf4ac63f
stepjue <stepjue@gmail.com>
2013-12-15 17:08:19 -0800

    much comment. wow

--- get_yahoo.py
+++ get_yahoo.py
@@ -1,14 +1,24 @@
-import feedparser # parses RSS feeds
-import string, random
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# by stepjue
+
+# Parses the Yahoo! Answers RSS feed, returns a random post
+
+import feedparser	# for parsing the RSS feed
+import string		# for Post.get_num_digits()
+import random 		# for Page.get_random_post()
 
 RSS_FEED = 'http://answers.yahoo.com/rss/allq'
+
 MAX_TWEET = 140
 MAX_DIGITS = 8
-
 COLON_LEN = len(' : ')
 
-# An object that represents an individual post
+# An object that represents an individual post.
 class Post(object):
+
+	# A post is initialized with a title, the part of the title that's the question,
+	# the length of the question, and the number of digits in the question.
 	def __init__(self, title):
 		self.title = title
 		self.question = self.get_question()
@@ -25,26 +35,32 @@ class Post(object):
 		start = self.title.find(' : ') + COLON_LEN
 		return self.title[start:]
 
+	# Twitter doesn't allow tweets over 140 characters.
 	def is_tweetable(self):
 		return self.length <= MAX_TWEET
 
-	def is_probably_not_spam(self):
-		return self.num_digits < MAX_DIGITS
+	# Spam posts tend to have a bunch of numbers in them, so if there are more
+	# than 8 digits, I'm marking the post as spam.
+	def is_spam(self):
+		return self.num_digits > MAX_DIGITS
 
-# an object that represents a list of tweetable posts
+# An object that represents a list of tweetable posts.
 class Page(object):
 	def __init__(self):
 		self.posts = self.get_posts()
 		self.num_posts = len(self.posts)
 
+	# Returns a list of Post objects scraped from the titles of
+	# the entries in the Yahoo! Answers RSS feed.
 	def get_posts(self):
 		d = feedparser.parse(RSS_FEED)
 		posts = []
 		for post in d.entries:
 			p = Post(post.title)
-			if p.is_tweetable() and p.is_probably_not_spam():
+			if p.is_tweetable() and not p.is_spam():
 				posts.append(p)
 		return posts
 
+	# Grabs a random post from the page
 	def get_random_post(self):
 		return self.posts[random.randrange(0,self.num_posts)].question
--- lifewhatdo.py
+++ lifewhatdo.py
@@ -2,27 +2,32 @@
 # -*- coding: utf-8 -*-
 # by stepjue
 
-# Tweets random/stupid/funny questions from Yahoo! Answers
+# Tweets random questions from Yahoo! Answers as @lifehowdo
 
 from get_yahoo import Page
 import tweepy, time, random
 
 HOUR = 3600 # in seconds
 
+# Twitter keys for OAuth
 CONSUMER_KEY = 'xxxxxxxxxx'
 CONSUMER_SECRET = 'xxxxxxxxxx'
 ACCESS_KEY = 'xxxxxxxxxx'
 ACCESS_SECRET = 'xxxxxxxxxx'
 
+# Authenticate as @lifehowdo
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# The main loop that tweets questions
 while True:
 	page = Page()
 	newpost = page.get_random_post()
-	print newpost
 	api.update_status(newpost)
 
-	# sleeps between 30 minutes and 3 hours between tweets
+	# Just for debugging
+	print(newpost)
+
+	# Sleeps between 30 minutes and 3 hours between tweets
 	time.sleep(random.randrange(HOUR/2, 3*HOUR))
